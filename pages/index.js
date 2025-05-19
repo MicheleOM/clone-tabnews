5603a88d5903b1388548a247a0243c7394a3404f
MicheleOM <115908319+MicheleOM@users.noreply.github.com>
2025-05-19 03:15:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1> Van Gogh, rust in vrede, mijn geliefde hond. 🐾❤️ </h1>
+  return <h1> Van Gogh, rust in vrede, mijn geliefde hond. 🐾❤️ </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
